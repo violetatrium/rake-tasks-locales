77507497bac6255f6cead22a0fb7630e814d0ce2
BSalemi <briandsalemi@gmail.com>
2021-11-18 11:34:31 -0500

    MIN-10339 built initial stages of rake gem

---
+++ lib/rake_locales.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rake_locales/version"
+
+module RakeLocales
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rake_locales/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RakeLocales
+  VERSION = "0.1.0"
+end
