cffb8b6a5510364f8f734bde697989be094d40ad
BSalemi <briandsalemi@gmail.com>
2022-11-17 12:02:21 -0500

    stringifying the jason

--- lib/minim_locales.rb
+++ lib/minim_locales.rb
@@ -186,7 +186,7 @@ module MinimLocales
       request["accept"] = 'application/vnd.api+json;profile="bulk"'
       request["content-type"] = 'application/vnd.api+json;profile="bulk"'
       request["authorization"] = "Bearer #{ENV['TRANSIFEX_BEARER_TOKEN']}"
-      request.body = "{\"data\":[{\"type\":\"resource_strings\"}, {\"strings\": \"#{locales_hash}\"}, {\"id\": \"#{ENV['TRANSLATE_ORG']}\"}]}"
+      request.body = JSON.stringify("{\"data\":[{\"type\":\"resource_strings\"}, {\"strings\": \"#{locales_hash}\"}, {\"id\": \"#{ENV['TRANSLATE_ORG']}\"}]}")
 
       response = http.request(request)
       byebug
